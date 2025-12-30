b1760f20ace87b55b4f78e0fa6553c536a141f03
lelekrecing <siipooke@gmail.com>
2025-12-30 11:19:34 +0700

    first commit

---
+++ app/Http/Controllers/Middleware/TrustProxies.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    // Trust semua proxy (Zeabur itu reverse proxy)
+    protected $proxies = '*';
+
+    // Trust semua forwarded headers (termasuk X-Forwarded-Proto https)
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
